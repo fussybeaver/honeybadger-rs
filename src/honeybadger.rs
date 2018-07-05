ee4144e3137d55cc34f7ab1e9f34d794a43aedb0
Niel Drummond <niel.drummond@fundingcircle.com>
2018-07-05 22:31:17 +0100

    Create initial tests and circleCI setup

--- src/honeybadger.rs
+++ src/honeybadger.rs
@@ -116,9 +116,9 @@ impl Honeybadger {
         })
     }
 
-    fn serialize<'req, E>(&self, 
-                       error: &E, 
-                       context: Option<HashMap<&'req str, &'req str>>) 
+    fn serialize<'req, E>(config: &Config,
+                          error: &E, 
+                          context: Option<HashMap<&'req str, &'req str>>) 
         -> serde_json::Result<Vec<u8>> 
         where E: ChainedError {
             
@@ -135,16 +135,16 @@ impl Honeybadger {
         };
 
         let server = notice::Server {
-            project_root: &self.config.root,
-            environment_name: &self.config.env,
-            hostname: &self.config.hostname,
+            project_root: &config.root,
+            environment_name: &config.env,
+            hostname: &config.hostname,
             time: SystemTime::now().duration_since(UNIX_EPOCH)
                 .map(|v| v.as_secs()).unwrap_or(0),
             pid: process::id()
         };
 
         let notice = Notice {
-            api_key: &self.config.api_key,
+            api_key: &config.api_key,
             notifier: notifier,
             error: error,
             request: request,
@@ -153,30 +153,39 @@ impl Honeybadger {
 
         serde_json::to_vec(&notice)
     }
+
+    fn create_payload_with_config<'req, E>(config: &Config,
+                                           user_agent: &str,
+                                           error: &E,
+                                           context: Option<HashMap<&'req str, &'req str>>) 
+        -> Result<Request<Body>>
+        where E: ChainedError {
+
+            let mut request = Request::builder();
+
+            let api_key: &str = config.api_key.as_ref();
+            let user_agent: &str = user_agent.as_ref();
+
+            request.uri(config.endpoint.clone())
+                .method(http::Method::POST)
+                .header(http::header::ACCEPT, "application/json")
+                .header("X-API-Key", api_key)
+                .header(http::header::USER_AGENT, user_agent);
+
+            let data = Honeybadger::serialize(config, error, context)?;
+
+            debug!("Serialized Honeybadger notify payload: {}", error);
+
+            let r = request.body(Body::from(data))?;
+            Ok(r)
+        }
     
     pub fn create_payload<'req, E>(&mut self, 
                              error: &E,
                              context: Option<HashMap<&'req str, &'req str>>) 
         -> Result<Request<Body>>
         where E: ChainedError {
-
-            let mut request = Request::builder();
-
-            let api_key: &str = self.config.api_key.as_ref();
-            let user_agent: &str = self.user_agent.as_ref();
-
-            request.uri(self.config.endpoint.clone())
-                .method(http::Method::POST)
-                .header(http::header::ACCEPT, "application/json")
-                .header("X-API-Key", api_key)
-                .header(http::header::USER_AGENT, user_agent);
-
-            let data = self.serialize(error, context)?;
-
-            debug!("Serialized Honeybadger notify payload: {}", error);
-
-            let r = request.body(Body::from(data))?;
-            Ok(r)
+            Honeybadger::create_payload_with_config(&self.config, &self.user_agent, error, context)
         }
 
     fn convert_error(kind: ErrorKind) -> Error {
@@ -187,17 +196,28 @@ impl Honeybadger {
     pub fn notify<'req>(&mut self, 
                         request: Request<Body>) -> impl Future<Item=(), Error=Error> {
 
+        Honeybadger::notify_with_client(&self.client, &self.config, &self.user_agent, request)
+    }
+
+    fn notify_with_client<'req, C>(client: &Client<C>,
+                                   config: &Config,
+                                   user_agent: &str,
+                                   request: Request<Body>) -> impl Future<Item=(), Error=Error>
+        where C: ::hyper::client::connect::Connect + Sync + 'static,
+              C::Error: 'static,
+              C::Transport: 'static {
+
         let now = ::std::time::Instant::now();
-        let t = self.config.timeout.as_secs();
+        let t = config.timeout.as_secs();
 
-        debug!("Sending honebadger payload with user agent: {}", self.user_agent);
+        debug!("Sending honebadger payload with user agent: {}", user_agent);
 
-        self.client.request(request)
+        client.request(request)
             .map_err(move |e| {
                 error!("Honeybadger client error: {}", e);
                 Honeybadger::convert_error(ErrorKind::Hyper(e))
             })
-            .deadline(now + self.config.timeout)
+            .deadline(now + config.timeout)
             .map_err(move |e| {
                 error!("Honeybadger request timed-out!: {}", e);
                 Honeybadger::convert_error(ErrorKind::TimeoutError(t))
@@ -218,5 +238,69 @@ impl Honeybadger {
                 }
             })
     }
-
 }
+
+#[cfg(test)]
+mod tests {
+
+    use honeybadger::{ConfigBuilder, Config, Honeybadger};
+    use hyper::Body;
+    use hyper::client::Client;
+    use hyper_mock::SequentialConnector;
+    use tokio::runtime::current_thread;
+    use errors::*;
+
+    fn test_client_with_response(res: String, config: &Config) -> Result<()> {
+        let mut c = SequentialConnector::default();
+        c.content.push(res);
+
+        let client = Client::builder()
+            .build::<SequentialConnector, Body>(c);
+
+        let mut rt = current_thread::Runtime::new().unwrap();
+
+        let error : Result<()> = Err(ErrorKind::RedirectionError.into());
+        let req = Honeybadger::create_payload_with_config(config, "test-client", &error.unwrap_err(), None).unwrap();
+        let res = Honeybadger::notify_with_client(&client, config, "test-client", req);
+
+        rt.block_on(res)
+    }
+
+    #[test]
+    fn test_notify_ok() {
+        let config = ConfigBuilder::new("dummy-api-key").build();
+        let res = test_client_with_response("HTTP/1.1 201 Created\r\n\
+                                             Server: mock1\r\n\
+                                             \r\n\
+                                             ".to_string(), &config);
+
+        assert_eq!((), res.unwrap());
+    }
+
+    #[test]
+    fn test_notify_timeout() {
+        let config = ConfigBuilder::new("dummy-api-key").build();
+        let res = test_client_with_response("HTTP/1.1 201 Created\r\n".to_string(), &config);
+
+        match res {
+            Err(Error(ErrorKind::TimeoutError(5), _)) => assert!(true),
+            _ => 
+                assert_eq!("", "expected timeout error, but was not")
+        }
+    }
+
+    #[test]
+    fn test_notify_rate_exceeded() {
+        let config = ConfigBuilder::new("dummy-api-key").build();
+        let res = test_client_with_response("HTTP/1.1 429 Too Many Requests\r\n\
+                                             Server: mock1\r\n\
+                                             \r\n\
+                                             ".to_string(), &config);
+
+        match res {
+            Err(Error(ErrorKind::RateExceededError, _)) => assert!(true),
+            _ => assert_eq!("", "expected timeout error, but was not")
+        }
+    }
+}
+
